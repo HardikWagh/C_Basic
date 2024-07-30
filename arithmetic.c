5ef1fe0df3238dff4472c21a5cb2c46f508e1225
HardikWagh <161699531+HardikWagh@users.noreply.github.com>
2024-07-30 09:48:20 +0530

    Create arithmetic.c
    Add File: arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("Addition of the numbers is: %d.\n", (a+b));
+    printf("Subraction of the numbers is: %d. \n", (a-b));
+    printf("Multiplication of the numbers is: %d. \n", (a*b));
+    printf("Division of the numbers is: %d.\n", (a/b));
+    return 0;
+}
